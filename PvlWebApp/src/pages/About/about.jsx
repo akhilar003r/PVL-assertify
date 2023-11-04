2a2dffe4d66b846167449c1e4f63e7eca0687629
akhilar003r <akilarajesh003@gmail.com>
2023-11-04 22:05:51 +0530

    about recycled back

---
+++ PvlWebApp/src/pages/About/about.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import background1 from '../../assets/bg1.jfif';
+import './about.css';
+
+const About = () => {
+  return (
+    <div className="about-container">
+      <div className='centre'>
+      <div className='centre'>
+        <h2>Welcome to Our About Page</h2>
+        <p>We are a dedicated team working towards...</p>
+
+        <h3>Meet Our Founder</h3>
+        <p>John Doe, Founder and CEO...</p>
+
+        <h3>Our Journey</h3>
+        <p>Founded in 20XX, our company has been on a mission to...</p>
+
+        <h3>Awards and Recognition</h3>
+        <p>Throughout our journey, we have been honored with...</p>
+
+        <h3>What Our Clients Say</h3>
+        <blockquote>"Working with Carol Solvay has been a game-changer for our business."</blockquote>
+
+        <h3>Future Goals</h3>
+        <p>Looking ahead, we aim to...</p>
+      </div>
+      
+         
+      </div>
+    </div>
+  );
+};
+
+export default About;
+
+
+
+
